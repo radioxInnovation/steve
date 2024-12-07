9743f29a18d5ce4b0192753c06394e1b1e237bfa
frettig-radiox <f.rettig@radiox-innovation.de>
2024-12-07 16:14:59 +0100

    zip support

--- steve.py
+++ steve.py
@@ -23,6 +23,7 @@ import base64
 import zipfile
 import io
 import shutil
+import uuid
 
 class Pipeline:
 
@@ -37,18 +38,24 @@ class Pipeline:
             print(f"{package} is not installed. Starting installation.")
             subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-    def spit_system_prompt( self, body ):
+    def spit_system_prompt( self, body, root ):
         system_template = next((msg["content"] for msg in body["messages"] if msg["role"] == "system"), "")
         header = {}
-        match = re.match(r"^##([^\n]+)##$", system_template) 
+
+        match = re.match(r"^data:application/zip;base64,([^\n]+)$", system_template) 
         if match:
-            extracted_path = match.group(1)
-            if os.path.exists( extracted_path ):
-                try:
-                    with open(extracted_path, 'r') as file:
-                        system_template = file.read()
-                except Exception as e:
-                    self.log( f"Failed to read file: {e}" )
+            base64_data = match.group(1)
+            file_data = base64.b64decode( base64_data )
+            system_template = ""
+
+            # extract
+            with zipfile.ZipFile(io.BytesIO(file_data)) as zip_file:
+                zip_file.extractall(path=root)
+
+                for file_name in zip_file.namelist():
+                    if file_name.endswith("system.mako"):
+                        with zip_file.open(file_name) as file:
+                            system_template = file.read().decode('utf-8') 
 
         yaml_match = re.match( r'---\s*\n(.*?)\n---\s*\n?(.*)', system_template, re.DOTALL )
         if yaml_match:
@@ -56,22 +63,26 @@ class Pipeline:
             system_template = yaml_match.group(2) if yaml_match.group(2) else ""
         return header, system_template
 
-    def render( self, system_template, header):
-        template = Template( system_template )
-        buf = io.StringIO()
-        ctx = Context(buf, **header )
-        template.render_context(ctx)
-        system_prompt = str ( buf.getvalue() )
+    def render( self, system_template, header, root):
+        try:
+            header["root"] = root
+            template = Template( system_template )
+            buf = io.StringIO()
+            ctx = Context(buf, **header )
+            template.render_context(ctx)
+            system_prompt = str ( buf.getvalue() )
 
-        function_names = [ "pipe", "inlet", "outlet"]
-        interface_functions = {}
+            function_names = [ "pipe", "inlet", "outlet"]
+            interface_functions = {}
 
-        for f in function_names:
-            if hasattr(ctx, f) and callable(getattr(ctx, f)):
-                interface_functions[f] = getattr(ctx, f)
-        return system_prompt, interface_functions
+            for f in function_names:
+                if hasattr(ctx, f) and callable(getattr(ctx, f)):
+                    interface_functions[f] = getattr(ctx, f)
+            return system_prompt, interface_functions
+        except Exception as e:
+            return f"Error processing template: {e}. Output this error massage at the beginning of each response", {}
 
-    def process_files( self, header):
+    def process_files( self, header, temp_dir_path ):
         files = {}
         try:
             if "files" in header:
@@ -79,10 +90,10 @@ class Pipeline:
                 for filename in files.keys():
                     file_info = files[ filename ]
                     if "url" in file_info:
-                        url = file_info['url']
-                        save =  file_info.get('save', True )
-                        overwrite = file_info.get('overwrite', True)
-                        extract = file_info.get('extract', True)
+                        url = file_info[ 'url' ]
+                        save =  file_info.get( 'save', True )
+                        overwrite = file_info.get( 'overwrite', True )
+                        extract = file_info.get( 'extract', True )
                         
                         try:
 
@@ -97,8 +108,9 @@ class Pipeline:
                                 file_data = response.content
 
                             if save:
-                                full_path = os.path.abspath( filename )
+                                full_path = os.path.abspath( os.path.join( temp_dir_path, filename) )
                                 path = os.path.dirname(full_path)
+                                file_info["path"] = path
 
                                 try:
                                     os.makedirs(path, exist_ok=True)
@@ -142,8 +154,11 @@ class Pipeline:
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
 
+        root = os.path.join("/app", "steve", str(uuid.uuid4()))
+        os.makedirs(root, exist_ok=True)
+
         try:
-            header, system_template = self.spit_system_prompt( body )
+            header, system_template = self.spit_system_prompt( body, root )
         except Exception as e:
             return self.log( f"Failed to parse yaml header: {e}" )
 
@@ -154,13 +169,10 @@ class Pipeline:
         except Exception as e:
             return self.log( f"Failed to install requirements {e}" )
 
-        files = self.process_files( header )
+        files = self.process_files( header, root )
 
-        try:
-            system, interface_functions = self.render( system_template, header )
-        except Exception as e:
-            return self.log( f"Failed to render template {e}" )
-
+        system, interface_functions = self.render( system_template, header, root )
+        
         body["messages"] = [message for message in body["messages"] if message["role"] != "system"]       
         body["messages"].insert(0, {"role": "system", "content": system } )
 
@@ -179,62 +191,42 @@ class Pipeline:
 
         try:
             response_class = interface_functions.get( "outlet", CustomResponse )
+
         except Exception as e:
             return self.log( f"Failed to find outlet class {e}" )
 
-        try:
-            pipe_func = interface_functions.get("pipe", None)
-            if pipe_func:
-                return pipe_func( user_message, model_id, messages, body )
-
-        except Exception as e:
-            return log (f"Failed to execute pipe function: {e}")
+        pipe_func = interface_functions.get("pipe", None)
 
         custom_response = response_class ( )
 
-        def call_ollama ( ollama_url, ollama_model, custom_response ):
+        if pipe_func:
+            for pipe_resp in pipe_func( user_message, model_id, messages, body ):
+                for resp in custom_response.process( pipe_resp ):
+                    yield resp
+                for resp in custom_response.final():
+                    yield resp
+
+        elif "ollama_url" in header and "model" in header:
+            OLLAMA_URL = header.get("ollama_url", "http://host.docker.internal:11434")
+            MODEL = header.get("model", "llama3.2:latest")
+
             from ollama import Client
             client = Client( host = ollama_url )
-            return client.chat(model=ollama_model, messages=body["messages"], stream=body["stream"] )
+            response = client.chat(model=ollama_model, messages=body["messages"], stream=body["stream"] )
 
-        def call_open_ai( url , model, open_ai_api_key, custom_response ):
-            from openai import OpenAI
-            client = OpenAI( api_key = open_ai_api_key )
-            return client.chat.completions.create( model=model, messages= body["messages"], stream=body["stream"] )
-
-        if "ollama_url" in header and "model" in header:
-            OLLAMA_URL = header.get("ollama_url", "http://host.docker.internal:11434")
-            MODEL = header.get("model", "llama3.1:latest")
-            response = call_ollama ( OLLAMA_URL, MODEL, custom_response )
-
-        elif "open_ai_api_key" in header and "model" in header:
-            url = header.get ( "url", "https://api.openai.com/v1/chat/completions" )
-            response = call_open_ai( header.get ( "url", "https://api.openai.com/v1/chat/completions" ), header["model"], header["open_ai_api_key"], custom_response )
-
-        if response:
-            custom_response = response_class ( )
             if body["stream"]:
                 for chunk in response:
                     try:
-                        try:
-                            line = chunk.choices[0].delta.content or ""
-                        except:
-                            line = chunk['message']['content'] or ""
+                        line = chunk['message']['content'] or ""
                         
                         if type( line ) == str:
                             for resp in custom_response.process( line ):
                                 yield resp
-                        try:
-                            if chunk["done"] == True:
-                                for resp in custom_response.final():
-                                    yield resp
-                        except:
-                            try:
-                                if chunk.choices[0].finish_reason == 'stop':
-                                    for resp in custom_response.final():
-                                        yield resp
-                            except:
-                                pass
+
+                        if chunk["done"] == True:
+                            for resp in custom_response.final():
+                                yield resp
+
                     except Exception as e:
                         yield f"Failed to process chunk {e}"
             else:
@@ -243,3 +235,36 @@ class Pipeline:
 
                 for resp in custom_response.final():
                     yield resp
+
+        elif "open_ai_api_key" in header and "model" in header:
+            url = header.get ( "url", "https://api.openai.com/v1/chat/completions" )
+            from openai import OpenAI
+            client = OpenAI( api_key = header["open_ai_api_key"] )
+
+            response = client.chat.completions.create( model=header["model"], messages= body["messages"], stream=body["stream"] )
+
+            if body["stream"]:
+                for chunk in response:
+                    try:
+                        line = chunk.choices[0].delta.content or ""
+                        
+                        if type( line ) == str:
+                            for resp in custom_response.process( line ):
+                                yield resp
+                        if chunk.choices[0].finish_reason == 'stop':
+                            for resp in custom_response.final():
+                                yield resp
+                    except Exception as e:
+                        yield f"Failed to process chunk {e}"
+            else:
+                for resp in custom_response.process( response.choices[0].message.content ):
+                    yield resp
+
+                for resp in custom_response.final():
+                    yield resp
+
+        else:
+            yield f"system: {system}"
+
+        #if os.path.exists(temp_dir_path):
+        #    shutil.rmtree(temp_dir_path)
